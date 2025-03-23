3a52dcf204a61124defc7eeae4a629a616fda7a6
DonatB <donatbajrami@hotmail.com>
2025-03-23 23:26:42 +0100

    Initial Commit

---
+++ CombineVincent/CombineVincentApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineVincentApp.swift
+//  CombineVincent
+//
+//  Created by Donat on 23.3.25.
+//
+
+import SwiftUI
+
+@main
+struct CombineVincentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
